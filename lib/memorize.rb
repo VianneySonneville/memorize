ad2c5428e5629833a7398b35954962b5db95124d
vianney.sonneville <vianney.sonneville@unova.fr>
2023-10-12 16:20:34 +0200

    base configuration

--- lib/memorize.rb
+++ lib/memorize.rb
@@ -1,23 +1,22 @@
 # frozen_string_literal: true
 
-require_relative "memorize/version"
+require_relative 'memorize/version'
 
 module Memorize
-  class Error < StandardError; end
+  # class Error < StandardError; end
+
   # Your code goes here...
   def self.included(klass)
     class << klass
       alias_method :__new, :new
       def new(*args)
-        e = __new $args
+        e = __new args
         e.after_init
 
         e
       end
     end
-
-    def after_init
-
-    end
   end
+
+  def after_init; end
 end
